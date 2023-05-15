f967b7569e0d1877ca55db5fc21570587eec1ec0
haariskhatri <haarishkkhatri@gmail.com>
2023-05-15 18:02:59 +0530

    removed unwanted code

--- billing/src/components/addressbar.jsx
+++ billing/src/components/addressbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AddressBar = ({ handlebillerchange, biller }) => {
 
-    console.log(biller.nameto);
+
     return (
         <>
             <div className="address-box">
