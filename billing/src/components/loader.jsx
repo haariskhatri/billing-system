003ad528930dcfd4edd66c7cde01cd5d5118b00f
haariskhatri <haarishkkhatri@gmail.com>
2023-05-16 18:38:22 +0530

    fixed errors

--- billing/src/components/loader.jsx
+++ billing/src/components/loader.jsx
@@ -5,10 +5,10 @@ const Preloader = () => {
         <>
             <div className="preloader-overlay">
 
-                <div class="typewriter">
-                    <div class="slide"><i></i></div>
-                    <div class="paper"></div>
-                    <div class="keyboard"></div>
+                <div className="typewriter">
+                    <div className="slide"><i></i></div>
+                    <div className="paper"></div>
+                    <div className="keyboard"></div>
                 </div>
             </div>
         </>
